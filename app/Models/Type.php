7ec6e36047ae97ef8dc54fcac39dec4b97046d29
orazi-paolo <orazi.paolo10@gmail.com>
2024-11-06 15:52:41 +0100

    create model type and define relationship one to many with projects

---
+++ app/Models/Type.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Project;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Type extends Model
+{
+    use HasFactory;
+
+    // definisco il rapporto uno a molti con i projects
+    public function projects()
+    {
+        return $this->hasMany(Project::class);
+    }
+}
