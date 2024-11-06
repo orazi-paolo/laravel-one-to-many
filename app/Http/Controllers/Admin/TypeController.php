14dacd2b8bfc4e14c79062c0b6eaf5c6306fdf0c
orazi-paolo <orazi.paolo10@gmail.com>
2024-11-06 18:09:44 +0100

    update method controller destroy

--- app/Http/Controllers/Admin/TypeController.php
+++ app/Http/Controllers/Admin/TypeController.php
@@ -65,8 +65,9 @@ class TypeController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Type $type)
     {
-        //
+        $type->delete();
+        return redirect()->route('admin.types.index');
     }
 }
